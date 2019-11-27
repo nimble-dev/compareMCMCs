740d78821eb14b088508f84bbf205d0a0aaad936
salleuska <sallypaganin@gmail.com>
2019-11-26 16:09:53 -0800

    ad hoc fixes to run stan via compareMCMCs

--- compareMCMCs/R/MCMCdef_stan.R
+++ compareMCMCs/R/MCMCdef_stan.R
@@ -1,3 +1,6 @@
+## SP: This can support running stan via compareMCMCs function 'standalone'
+## I don't think that this would work with multiple MCMCs engines by now
+
 MCMCdef_stan <- function(MCMCinfo, 
                         MCMCcontrol, 
                         monitorInfo, 
@@ -13,86 +16,102 @@ MCMCdef_stan_impl <- function(MCMCinfo,
                               monitorInfo, 
                               modelInfo) {
   ## SP: modelInfo is list containing
-  ## model: name of .stan file containing the modelcode 
-  ## data: should be already in long format?
-  ## init: optional initial values 
+  ## - stan_model: name of .stan file containing the modelcode  ## SP: was only model, but then it is rewritten form compareMCMCs function
+  ## - data: should be already in long format?
+  ## - init: name of a file with initial values in the long format required by Stan
 
-  stanInfo <- modelInfo[['stan']]
+  stanInfo <- modelInfo
+  
   if(is.null(stanInfo))
     stop("stan MCMC was requested but there is no stan entry in modelInfo.")
   if(requireNamespace('rstan', quietly = TRUE)) {
-    stop("stan MCMC was requested but but the rstan package is not installed.")
-  }
-  stan_model <- stanInfo$model
-  if(is.null(stan_model) | stan_model == '')
-    stop('must provide \'model\' argument to run Stan MCMC')
-  dataFile <- stanInfo$data
-  if(is.null(dataFile))
-    stop("stan entry in modelInfo is missing a data entry.")
-  if(!is.list(dataFile))
-    constantsAndDataStan <- fileToList(dataFile)
-  else
-    constantsAndDataStan <- dataFile
+    
+    stan_model <- stanInfo$stan_model
+    if(is.null(stan_model) | stan_model == '')
+      stop('must provide \'model\' argument to run Stan MCMC')
+    
+    dataFile <- stanInfo$data
+    
+    if(is.null(dataFile))
+      stop("stan entry in modelInfo is missing a data entry.")
+    
+    if(!is.list(dataFile)){
+      
+      stop("need to pass data in stan supported format")
+      ## SP: fileToList not found
+      # constantsAndDataStan <- fileToList(dataFile)
+   
+    } else {
+      constantsAndDataStan <- dataFile
+    }
 
-  initFile <- stanInfo$init
-  if(is.null(initFile))
-    stop("stan entry in modelInfo is missing an init entry.")
-  if(!is.list(initFile)) {
-    if(file.exists(initFile))
-      initsStan <- fileToList(initFile)
-    else
-      initsStan <- NULL
-  } else
-    initsStan <- initFile
+    initFile <- stanInfo$init
+    if(is.null(initFile))
+      stop("stan entry in modelInfo is missing an init entry.")
+    if(!is.list(initFile)) {
+      if(file.exists(initFile)){
+        initsStan <- fileToList(initFile)
+      } else {
+        initsStan <- NULL
+      }
+    } else {
+      initsStan <- initFile
+    }
 
-  compileTime <- system.time(stan_mod <- rstan::stan_model(file = stan_model))
+    compileTime <- system.time(stan_mod <- rstan::stan_model(file = stan_model))
 
-  if(is.null(initsStan)) {
-    ## missing model.init.R file (stan inits file)
-    runTime <- system.time(
-      stan_out <- rstan::sampling(stan_mod,
-                                  data=constantsAndDataStan,
-                                  chains=1,
-                                  iter=MCMCcontrols$niter,
-                                  thin=MCMCcontrols$thin))
+    ## SP: using the burnin as warmup but may be wrong
+    if(is.null(initsStan)) {
+      ## missing model.init.R file (stan inits file)
+      runTime <- system.time(
+        stan_out <- rstan::sampling(stan_mod,
+                                    data=constantsAndDataStan,
+                                    chains=1,
+                                    iter= 2*MCMCcontrol$niter,
+                                    thin=MCMCcontrol$thin))
+    } else {
+      ## we have the model.init.R file
+      ## this one includes inits = ...
+      runTime <- system.time(
+        stan_out <- rstan::sampling(stan_mod,
+                                    data=constantsAndDataStan,
+                                    chains=1,
+                                    iter= 2*MCMCcontrol$niter,
+                                    thin=MCMCcontrol$thin,
+                                    init=list(initsStan)))
+    }
+
+
+    tempArray <- rstan::extract(stan_out,
+                                permuted = FALSE,
+                                inc_warmup = TRUE)[, 1, ]
+    
+    ## sampling time, discarding warmup
+    ## SP: rstan::get_elapsed_time() returns time in a different format from system.type, so I am just putting some zeros to maintain the format across different types of MCMCresults
+
+    timeResult <- c(0, 0, rstan::get_elapsed_time(stan_out)[2]) 
+
+    monitors <- MCMCcontrol$monitors
+    if(!all(monitors %in% dimnames(tempArray)[[2]])) {
+      missingNames <- setdiff(monitors, dimnames(tempArray)[[2]])
+      warning(paste0('Stan output is missing values for: ',
+                     paste0(missingNames,collapse=', ')))
+    }
+    # samplesArray <- array(0, dim = c(nkeep, length(monitors)))
+    # dimnames(samplesArray)[[2]] <- monitors
+    
+    monitorsWeHave <- intersect(monitors, dimnames(tempArray)[[2]])
+    samplesArray <- tempArray[(MCMCcontrol$niter+1):floor((2*MCMCcontrol$niter)/MCMCcontrol$thin),
+                               monitorsWeHave,
+                               drop=FALSE]
+    
+    ## return MCMCresult object, with samples and time populated                                         
+    result <- MCMCresult$new(samples = samplesArray,
+                               times = list(sample = timeResult))
+    # unlink(modelFileName)
+    return(result)
   } else {
-    ## we have the model.init.R file
-    ## this one includes inits = ...
-    runTime <- system.time(
-      stan_out <- rstan::sampling(stan_mod,
-                                  data=constantsAndDataStan,
-                                  chains=1,
-                                  iter=MCMCcontrols$niter,
-                                  thin=MCMCcontrols$thin,
-                                  init=list(initsStan)))
+    stop("stan MCMC was requested but the rstan package is not installed.")
+    return(NULL)
   }
-
-
-  tempArray <- rstan::extract(stan_out,
-                              permuted = FALSE,
-                              inc_warmup = FALSE)[, 1, ]
-  
-  ## sampling time, discarding warmup
-  ## SP: rstan::get_elapsed_time() returns time in a different format from system.type, so I am just putting some 0 before to maintain the format across different types of MCMCresults
-
-  timeResult <- c(0, 0, rstan::get_elapsed_time(stan_out)[2]) 
-
-  monitors <- MCMCcontrols$monitors
-  if(!all(monitors %in% dimnames(tempArray)[[2]])) {
-    missingNames <- setdiff(monitors, dimnames(tempArray)[[2]])
-    warning(paste0('Stan output is missing values for: ',
-                   paste0(missingNames,collapse=', ')))
-  }
-  samplesArray <- array(0, dim = c(nkeep, length(monitors)))
-  dimnames(samplesArray)[[2]] <- monitors
-  monitorsWeHave <- intersect(monitors, dimnames(tempArray)[[2]])
-  samplesArray[, monitorsWeHave] <- tempArray[1:floor(niter/thin),
-                                              monitorsWeHave,
-                                              drop=FALSE]
-  
-  ## return MCMCresult object, with samples and time populated                                         
-  result <- MCMCresult$new(samples = samplesArray,
-                             times = list(sample = timeResult))
-  # unlink(modelFileName)
-  return(result)
 }
