044b73d2335299f50184f67f2a66899534cf6672
salleuska <sallypaganin@gmail.com>
2020-01-21 12:55:51 -0800

    stan support for compareMCMCs

--- compareMCMCs/R/MCMCdef_stan.R
+++ compareMCMCs/R/MCMCdef_stan.R
@@ -1,6 +1,3 @@
-## SP: This can support running stan via compareMCMCs function 'standalone'
-## I don't think that this would work with multiple MCMCs engines by now
-
 MCMCdef_stan <- function(MCMCinfo, 
                         MCMCcontrol, 
                         monitorInfo, 
@@ -42,7 +39,7 @@ MCMCdef_stan_impl <- function(MCMCinfo,
   ##  
   
   	if(is.null(MCMCinfo))
-   		stop("stan MCMC was requested but there is no stan entry in modelInfo.")
+   		stop("stan MCMC was requested but there is no stan entry in externalMCMCinfo")
   	if(requireNamespace('rstan', quietly = TRUE)) {
     
     ## extract base elements from MCMCInfo
@@ -50,32 +47,20 @@ MCMCdef_stan_impl <- function(MCMCinfo,
     dataStan <- MCMCinfo$data
     initStan <- MCMCinfo$init
 
-    if(is.null(fileStan) | fileStan == '')
-      stop('must provide \'file\' argument to run Stan MCMC')
+    if(is.null(fileStan) | fileStan == '') stop('must provide \'file\' argument to run Stan MCMC')
     
-    if(is.null(dataStan))
-      stop("stan entry in modelInfo is missing a data entry.")
+    if(is.null(dataStan)) stop("stan entry in externalMCMCinfo is missing a data entry.")
     
-    if(!is.list(dataStan))
-      stop("need to pass data in stan supported format")   
- 
-    ## SP: stan has a default random initialization:
-    ## 1) should we allow for that?
-  	## 2) if yes, should we give a watrning message?
-
-    # if(is.null(initStan))
-    #   stop("stan entry in modelInfo is missing an init entry.")
-    
-    if(!is.list(initStan)) 
-      stop("stan entry in modelInfo is missing a data entry.")
-     
+    if(!is.list(dataStan)) stop("need to pass data in stan supported format")   
+      
     ## extract general elements from MCMCInfo
   	stan_model_args <- if(!is.null(MCMCinfo$stan_model_args)) MCMCinfo$stan_model_args else list() 
   	sampling_args   <- if(!is.null(MCMCinfo$sampling_args))   MCMCinfo$sampling_args   else list()
 
   	## modify stan_model_args
 	stan_model_args$file <- fileStan
-    compileTime <- system.time(stan_mod <- do.call(rstan::stan_model, stan_model_args))
+	## Create stan_model object
+	compileTime <- system.time(stan_mod <- do.call(rstan::stan_model, stan_model_args))
 
   	## modify sampling args
 	sampling_args$object <- stan_mod ## object of class stanmodel
@@ -83,15 +68,22 @@ MCMCdef_stan_impl <- function(MCMCinfo,
     sampling_args$chains <- 1
 
     ##  Note: in rstan::sampling function the `iter` argument comprises also the number of warmup iterations
-    sampling_args$iter   <- MCMCcontrol$niter*2  ## SP: temp choice, using default stan warmup (half of niter)
+    sampling_args$iter   <- MCMCcontrol$niter*2  ## SP: using default stan warmup (half of niter)
     sampling_args$warmup <- floor(MCMCcontrol$niter/2)
     sampling_args$thin   <- MCMCcontrol$thin
     sampling_args$seed   <- MCMCcontrol$seed
+    
+    ## SP: we could have the monitors passed to rstan:::sampling function
+    # sampling_args$pars <- MCMCcontrol$monitors
 
-
-    if(is.null(initsStan)) {
+    ## Should 
+    # sampling_args$pars   <- MCMCcontrol$monitors
+    ## SP: stan has a default random initialization:
+    ## 1) should we allow for that? 
+  	## 2) if yes, should we give a warning message?
+    
+    if(is.null(initStan)) {
       ## missing init (uses stan random initialization)
-      ## 
       runTime <- system.time(
         stan_out <- do.call(rstan::sampling, sampling_args))
     } else {
@@ -103,40 +95,36 @@ MCMCdef_stan_impl <- function(MCMCinfo,
         stan_out <- do.call(rstan::sampling, sampling_args))
     }
 
+    ## SP: should we include warmup samples?  
     tempArray <- rstan::extract(stan_out,
                                 permuted = FALSE,
-                                inc_warmup = TRUE)[, 1, ]
+                                inc_warmup = FALSE)[, 1, ]
     
-    ### ---- to discuss ---- #
     ## SP: rstan::get_elapsed_time() returns time in a different format from system.time,
-    ## so I am just putting some zeros to maintain the format across different types of MCMCresults
+    ## I am just adding some zeros to maintain the format across different types of MCMCresults
     ## sampling time, discarding warmup
+ 
+    sampleTime <- c(0, 0, rstan::get_elapsed_time(stan_out)[2]) 
+    totalTime  <- runTime
 
-    timeResult <- c(0, 0, rstan::get_elapsed_time(stan_out)[2]) 
-    ### ---- end to discuss ---- #
-
-    monitors <- MCMCcontrol$monitors
+    monitors <- monitorInfo$monitors
+    
     if(!all(monitors %in% dimnames(tempArray)[[2]])) {
       missingNames <- setdiff(monitors, dimnames(tempArray)[[2]])
       warning(paste0('Stan output is missing values for: ',
                      paste0(missingNames,collapse=', ')))
     }
-    # samplesArray <- array(0, dim = c(nkeep, length(monitors)))
-    # dimnames(samplesArray)[[2]] <- monitors
+    dimnames(samplesArray)[[2]] <- monitors
     
     monitorsWeHave <- intersect(monitorInfo$monitors, dimnames(tempArray)[[2]])
     
-    ### ---- to discuss ---- #
-    ## SP: doubling up niter since stan by default uses 
-    samplesArray <- tempArray[(MCMCcontrol$niter+1):floor((2*MCMCcontrol$niter)/MCMCcontrol$thin),
-                               monitorsWeHave,
+    samplesArray <- tempArray[, monitorsWeHave,
                                drop=FALSE]
-    ### ---- end to discuss ---- #
+
 
     ## return MCMCresult object, with samples and time populated                                         
     result <- MCMCresult$new(samples = samplesArray,
-                               times = list(sample = timeResult))
-    # unlink(modelFileName)
+                               times = list(sample = sampleTime, total = totalTime, compile = compileTime))
     return(result)
   } else {
     stop("stan MCMC was requested but the rstan package is not installed.")
