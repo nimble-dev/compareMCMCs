5bf56848e4712cd318332646face012dec6ff291
danielturek <danielturek@gmail.com>
2021-12-04 19:11:12 -0500

    added definition for jags burninTimeResult when burnin=0

--- compareMCMCs/R/MCMCdef_jags.R
+++ compareMCMCs/R/MCMCdef_jags.R
@@ -29,6 +29,7 @@ MCMCdef_jags_impl <- function(MCMCinfo,
     })
     setupTime <- setupTimeResult[3]
     
+    burninTimeResult <- system.time(0)   ## needs to be defined even when burnin = 0
     burninTime <- 0
     if(MCMCcontrol$burnin > 0) {
         burninTimeResult <- system.time(update(object = jags_mod,
